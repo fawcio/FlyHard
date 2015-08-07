418644466fe4602d392ea940d5c4c8104e46ccf3
Sławomir Cielepak <slawomir.cielepak@gmail.com>
2015-08-07 23:51:23 +0200

    Clean up of cmake shit; Added portable library search and linking

--- cmake/cmake_defines.cmake
+++ cmake/cmake_defines.cmake
@@ -5,7 +5,7 @@ macro( CreateProject project_name project_main_src_file)
 
 project( ${project_name} )
 
-if( NOT ${CMAKE_BUILD_TYPE})
+if( NOT ${CMAKE_BUILD_TYPE} OR ${CMAKE_BUILD_TYPE} STREQUAL "" )
   set( CMAKE_BUILD_TYPE Debug )
 endif()
 
@@ -13,15 +13,24 @@ message( STATUS "Configuring project ${project_name} for ${CMAKE_BUILD_TYPE} bui
 
 if( ${CMAKE_BUILD_TYPE} MATCHES Debug )
   set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O0 -g3 -fstack-protector-all -march=native -std=c++11 -Wall -Wextra -pedantic -Werror" )
-elseif( ${CMAKE_BUILD_TYPE} MATCHES "Release" )
+elseif( ${CMAKE_BUILD_TYPE} MATCHES Release )
   set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -march=native -std=c++11 -Wall -Wextra -pedantic -Werror")
 endif()
 
-include_directories( ./src )
+FindRequiredLibraries()
+
+include_directories( src )
 
 add_executable( ${project_name} ${project_main_src_file} )
 
+SymLinkResources()
+
 endmacro( CreateProject )
+
+
+
+
+
 
 
 ##
@@ -32,9 +41,14 @@ message( STATUS "Linking modules with project ${project_name}" )
 
 target_link_libraries( ${project_name}
   -Wl,--start-group ${PROJECT_MODULES} -Wl,--end-group
+  ${EXTERNAL_LIBRARIES}
 )
 
 endmacro( BuildProject )
+
+
+
+
 
 
 ##
@@ -53,13 +67,45 @@ add_library( ${module_name} STATIC ${SRC_FILES} )
 endmacro( AddStaticModule )
 
 
+
+
+
+
+
+##
+# Find for required libraries
+##
+macro( FindRequiredLibraries )
+
+set(THREADS_PREFER_PTHREAD_FLAG ON)
+
+find_package(Threads REQUIRED)
+find_package(Boost 1.55 REQUIRED system)
+find_package(SFML  2.3  REQUIRED system window graphics network audio)
+find_package(X11   1.6  REQUIRED)
+
+set(EXTERNAL_LIBRARIES
+  Threads::Threads
+  ${SFML_LIBRARIES}
+  ${Boost_LIBRARIES}
+  ${X11_LIBRARIES}
+)
+
+endmacro( FindRequiredLibraries )
+
+
+
+
+
+
+
 ##
 # Create a symbolic link to Resources.
 ##
-macro( LinkResources )
-message( STATUS "Linking Resources to build directory ${CMAKE_BINARY_DIR}" )
+macro( SymLinkResources )
+message( STATUS "Creating a symlink to Resources in build directory ${CMAKE_BINARY_DIR}" )
 execute_process(
   COMMAND /usr/bin/ln -sf ${CMAKE_SOURCE_DIR}/Resources ${CMAKE_BINARY_DIR}/
   WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
 )
-endmacro( LinkResources )
+endmacro( SymLinkResources )
