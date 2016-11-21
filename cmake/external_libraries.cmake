db0ba76e51da9cf068a345bd9a28cd6fdaf400d5
Sławomir Cielepak <slawomir.cielepak@gmail.com>
2016-11-21 16:41:51 +0100

    First GTests

--- cmake/external_libraries.cmake
+++ cmake/external_libraries.cmake
@@ -9,9 +9,10 @@ set(THREADS_PREFER_PTHREAD_FLAG ON)
 set(CMAKE_MODULE_PATH /usr/share/SFML/cmake/Modules/)
 
 find_package(Threads REQUIRED)
-find_package(Boost 1.58 REQUIRED system program_options)
+find_package(Boost 1.59 REQUIRED system program_options)
 find_package(SFML  2.3  REQUIRED system window graphics network audio)
 find_package(X11   1.6  REQUIRED)
+find_package(GTest 1.7 REQUIRED)
 
 set(EXTERNAL_LIBRARIES
   Threads::Threads
---
+++ src/utils/tests/UnitsTests.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+#include "../Units.hpp"
+
+TEST(UtilsTests, PositiveTest1)
+{
+	ASSERT_TRUE(true);
+}
